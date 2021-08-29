66881d92a6d6df86d18735ba42267fa854861238
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-08-29 16:18:00 +0600

    first commit

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import ReactDOM from 'react-dom';
+import { Fragment } from 'react-is';
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onCloseCart} />;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    );
+};
+
+const Modal = (props) => {
+    const portalElement = document.getElementById('overlay');
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onCloseCart={props.onCloseCart} />,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+        </Fragment>
+    );
+};
+
+export default Modal;
