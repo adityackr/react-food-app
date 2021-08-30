820c5805280e0f8778cced0d377e34fe21219808
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-08-30 13:37:15 +0600

    use context API in stead of refs

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,97 +1,117 @@
-import { useRef, useState } from 'react';
+import useInput from '../../hooks/use_input';
 import classes from './Checkout.module.css';
 
-const isEmpty = (value) => value.trim() === '';
+const isNotEmpty = (value) => value.trim() !== '';
 const isElevenDigits = (value) => value.trim().length === 11;
 
 const Checkout = (props) => {
-    const [formInputsValidity, setFormInputsValidity] = useState({
-        name: true,
-        address: true,
-        phone: true,
-        city: true,
-    });
-    const nameInputRef = useRef();
-    const addressInputRef = useRef();
-    const phoneInputRef = useRef();
-    const cityInputRef = useRef();
+    const {
+        value: nameInputValue,
+        isValid: nameIsValid,
+        hasError: nameHasError,
+        valueChangeHandler: nameChangeHandler,
+        inputBlurHandler: nameInputBlurHandler,
+    } = useInput(isNotEmpty);
+
+    const {
+        value: addressInputValue,
+        isValid: addressIsValid,
+        hasError: addressHasError,
+        valueChangeHandler: addressChangeHandler,
+        inputBlurHandler: addressInputBlurHandler,
+    } = useInput(isNotEmpty);
+
+    const {
+        value: phoneInputValue,
+        isValid: phoneIsValid,
+        hasError: phoneHasError,
+        valueChangeHandler: phoneChangeHandler,
+        inputBlurHandler: phoneInputBlurHandler,
+    } = useInput(isElevenDigits);
+
+    const {
+        value: cityInputValue,
+        isValid: cityIsValid,
+        hasError: cityHasError,
+        valueChangeHandler: cityChangeHandler,
+        inputBlurHandler: cityInputBlurHandler,
+    } = useInput(isNotEmpty);
+
+    let formIsValid = false;
+
+    if (nameIsValid && addressIsValid && phoneIsValid && cityIsValid) {
+        formIsValid = true;
+    }
 
     const confirmHandler = (event) => {
         event.preventDefault();
-
-        const enteredName = nameInputRef.current.value;
-        const enteredAddress = addressInputRef.current.value;
-        const enteredPhone = phoneInputRef.current.value;
-        const enteredCity = cityInputRef.current.value;
-
-        const enteredNameIsValid = !isEmpty(enteredName);
-        const enteredAddressIsValid = !isEmpty(enteredAddress);
-        const enteredPhoneIsValid = isElevenDigits(enteredPhone);
-        const enteredCityIsValid = !isEmpty(enteredCity);
-
-        setFormInputsValidity({
-            name: enteredNameIsValid,
-            address: enteredAddressIsValid,
-            phone: enteredPhoneIsValid,
-            city: enteredCityIsValid,
-        });
-
-        const formIsValid =
-            enteredNameIsValid &&
-            enteredAddressIsValid &&
-            enteredPhoneIsValid &&
-            enteredCityIsValid;
 
         if (!formIsValid) {
             return;
         }
 
         props.onConfirm({
-            name: enteredName,
-            address: enteredAddress,
-            phone: enteredPhone,
-            city: enteredCity,
+            name: nameInputValue,
+            address: addressInputValue,
+            phone: phoneInputValue,
+            city: cityInputValue,
         });
     };
 
     const nameInputClasses = `${classes.control} ${
-        formInputsValidity.name ? '' : classes.invalid
+        !nameHasError ? '' : classes.invalid
     }`;
     const addressInputClasses = `${classes.control} ${
-        formInputsValidity.address ? '' : classes.invalid
+        !addressHasError ? '' : classes.invalid
     }`;
     const phoneInputClasses = `${classes.control} ${
-        formInputsValidity.phone ? '' : classes.invalid
+        !phoneHasError ? '' : classes.invalid
     }`;
     const cityInputClasses = `${classes.control} ${
-        formInputsValidity.city ? '' : classes.invalid
+        !cityHasError ? '' : classes.invalid
     }`;
 
     return (
         <form className={classes.form} onSubmit={confirmHandler}>
             <div className={nameInputClasses}>
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" ref={nameInputRef} />
-                {!formInputsValidity.name && <p>Please enter a valid name</p>}
+                <input
+                    type="text"
+                    id="name"
+                    onChange={nameChangeHandler}
+                    onBlur={nameInputBlurHandler}
+                />
+                {nameHasError && <p>Please enter a valid name</p>}
             </div>
             <div className={addressInputClasses}>
                 <label htmlFor="address">Address</label>
-                <input type="text" id="address" ref={addressInputRef} />
-                {!formInputsValidity.address && (
-                    <p>Please enter a valid address</p>
-                )}
+                <input
+                    type="text"
+                    id="address"
+                    onChange={addressChangeHandler}
+                    onBlur={addressInputBlurHandler}
+                />
+                {addressHasError && <p>Please enter a valid address</p>}
             </div>
             <div className={phoneInputClasses}>
                 <label htmlFor="phone">Phone Number</label>
-                <input type="text" id="phone" ref={phoneInputRef} />
-                {!formInputsValidity.phone && (
-                    <p>Please enter a valid phone number</p>
-                )}
+                <input
+                    type="text"
+                    id="phone"
+                    onChange={phoneChangeHandler}
+                    onBlur={phoneInputBlurHandler}
+                />
+                {phoneHasError && <p>Please enter a valid phone number</p>}
             </div>
             <div className={cityInputClasses}>
                 <label htmlFor="city">City</label>
-                <input type="text" id="city" ref={cityInputRef} />
-                {!formInputsValidity.city && <p>Please enter a valid city</p>}
+                <input
+                    type="text"
+                    id="city"
+                    onChange={cityChangeHandler}
+                    onBlur={cityInputBlurHandler}
+                />
+                {cityHasError && <p>Please enter a valid city</p>}
             </div>
             <div className={classes.actions}>
                 <button type="button" onClick={props.onCancel}>
