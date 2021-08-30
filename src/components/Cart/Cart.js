10d52c3db2b13d70f355c2ee13921c96c3eea3db
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-08-30 12:59:11 +0600

    Finished app

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
@@ -7,6 +7,9 @@ import Checkout from './Checkout';
 
 const Cart = (props) => {
     const [isOrdered, setIsOrdered] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
+    const [error, setError] = useState();
 
     const cartCtx = useContext(CartContext);
 
@@ -24,6 +27,31 @@ const Cart = (props) => {
 
     const orderHandler = () => {
         setIsOrdered(true);
+    };
+
+    const submitHandler = async (userData) => {
+        setIsSubmitting(true);
+        try {
+            const response = await fetch(
+                'https://react-food-app-c4146-default-rtdb.firebaseio.com/orders.json',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        user: userData,
+                        orderedItems: cartCtx.items,
+                    }),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+            setDidSubmit(true);
+            cartCtx.clearCart();
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsSubmitting(false);
     };
 
     const cartItems = (
@@ -57,15 +85,61 @@ const Cart = (props) => {
         </div>
     );
 
-    return (
-        <Modal onCloseCart={props.onCloseCart}>
+    const modalContents = (
+        <React.Fragment>
             {cartItems}
             <div className={classes.total}>
                 <span>Total</span>
                 <span>{totalAmount}</span>
             </div>
-            {isOrdered && <Checkout onCancel={props.onCloseCart} />}
+            {isOrdered && (
+                <Checkout
+                    onConfirm={submitHandler}
+                    onCancel={props.onCloseCart}
+                />
+            )}
             {!isOrdered && modalActions}
+        </React.Fragment>
+    );
+
+    const isSubmittingModalContent = (
+        <p>
+            <strong>Please wait...</strong>
+        </p>
+    );
+
+    const didSubmitModalContent = (
+        <React.Fragment>
+            <p>
+                <strong>Your order has successfully submitted!</strong>
+            </p>
+            <div className={classes.actions}>
+                <button className={classes.button} onClick={props.onCloseCart}>
+                    Close
+                </button>
+            </div>
+        </React.Fragment>
+    );
+
+    const errorContent = (
+        <React.Fragment>
+            <p className={classes.error}>
+                <strong>{error}</strong>
+            </p>
+            <div className={classes.actions}>
+                <button className={classes.button} onClick={props.onCloseCart}>
+                    Close
+                </button>
+            </div>
+        </React.Fragment>
+    );
+
+    return (
+        <Modal onCloseCart={props.onCloseCart}>
+            {!isSubmitting && !didSubmit && !error && modalContents}
+            {isSubmitting && isSubmittingModalContent}
+            {!isSubmitting && didSubmit && didSubmitModalContent}
+            {error && !didSubmit && errorContent}
         </Modal>
     );
 };
--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -2,58 +2,66 @@ import { useRef, useState } from 'react';
 import classes from './Checkout.module.css';
 
 const isEmpty = (value) => value.trim() === '';
+const isElevenDigits = (value) => value.trim().length === 11;
 
 const Checkout = (props) => {
     const [formInputsValidity, setFormInputsValidity] = useState({
         name: true,
-        house: true,
-        street: true,
+        address: true,
+        phone: true,
         city: true,
     });
     const nameInputRef = useRef();
-    const houseInputRef = useRef();
-    const streetInputRef = useRef();
+    const addressInputRef = useRef();
+    const phoneInputRef = useRef();
     const cityInputRef = useRef();
 
     const confirmHandler = (event) => {
         event.preventDefault();
 
         const enteredName = nameInputRef.current.value;
-        const enteredHouse = houseInputRef.current.value;
-        const enteredStreet = streetInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredPhone = phoneInputRef.current.value;
         const enteredCity = cityInputRef.current.value;
 
         const enteredNameIsValid = !isEmpty(enteredName);
-        const enteredHouseIsValid = !isEmpty(enteredHouse);
-        const enteredStreetIsValid = !isEmpty(enteredStreet);
+        const enteredAddressIsValid = !isEmpty(enteredAddress);
+        const enteredPhoneIsValid = isElevenDigits(enteredPhone);
         const enteredCityIsValid = !isEmpty(enteredCity);
 
         setFormInputsValidity({
             name: enteredNameIsValid,
-            house: enteredHouseIsValid,
-            street: enteredStreetIsValid,
+            address: enteredAddressIsValid,
+            phone: enteredPhoneIsValid,
             city: enteredCityIsValid,
         });
 
         const formIsValid =
             enteredNameIsValid &&
-            enteredHouseIsValid &&
-            enteredStreetIsValid &&
+            enteredAddressIsValid &&
+            enteredPhoneIsValid &&
             enteredCityIsValid;
 
         if (!formIsValid) {
             return;
         }
+
+        props.onConfirm({
+            name: enteredName,
+            address: enteredAddress,
+            phone: enteredPhone,
+            city: enteredCity,
+        });
     };
 
     const nameInputClasses = `${classes.control} ${
         formInputsValidity.name ? '' : classes.invalid
     }`;
-    const houseInputClasses = `${classes.control} ${
-        formInputsValidity.house ? '' : classes.invalid
+    const addressInputClasses = `${classes.control} ${
+        formInputsValidity.address ? '' : classes.invalid
     }`;
-    const streetInputClasses = `${classes.control} ${
-        formInputsValidity.street ? '' : classes.invalid
+    const phoneInputClasses = `${classes.control} ${
+        formInputsValidity.phone ? '' : classes.invalid
     }`;
     const cityInputClasses = `${classes.control} ${
         formInputsValidity.city ? '' : classes.invalid
@@ -66,18 +74,18 @@ const Checkout = (props) => {
                 <input type="text" id="name" ref={nameInputRef} />
                 {!formInputsValidity.name && <p>Please enter a valid name</p>}
             </div>
-            <div className={houseInputClasses}>
-                <label htmlFor="house">House Name</label>
-                <input type="text" id="house" ref={houseInputRef} />
-                {!formInputsValidity.house && (
-                    <p>Please enter a valid house name</p>
+            <div className={addressInputClasses}>
+                <label htmlFor="address">Address</label>
+                <input type="text" id="address" ref={addressInputRef} />
+                {!formInputsValidity.address && (
+                    <p>Please enter a valid address</p>
                 )}
             </div>
-            <div className={streetInputClasses}>
-                <label htmlFor="street">Street</label>
-                <input type="text" id="street" ref={streetInputRef} />
-                {!formInputsValidity.street && (
-                    <p>Please enter a valid street</p>
+            <div className={phoneInputClasses}>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="text" id="phone" ref={phoneInputRef} />
+                {!formInputsValidity.phone && (
+                    <p>Please enter a valid phone number</p>
                 )}
             </div>
             <div className={cityInputClasses}>
